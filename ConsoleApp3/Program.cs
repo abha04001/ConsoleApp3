092a3b1faab361da702415e90048ff84c6a59217
caspian <casa04001@utb.vaxjo.se>
2022-11-18 14:56:32 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace test
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Skriv en tal som är störa än 5");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x < 5)
+                Console.WriteLine("Det är fel");
+            else
+                Console.WriteLine("Det är rätt");
+        }
+    }
+
+} 
